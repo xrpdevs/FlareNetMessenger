75eb3c11ea17ccd3deab89dc1ae63c791b619b91
xrpdevs <xrpdevs@xrpdevs.co.uk>
2021-10-22 09:46:45 +0100

    1: App PIN is now one-way hashed with Scrypt. Wallet Private keys will be stored encrypted with a private key derived from the plaintext PIN and the PIN hash. The user will have to re-generate the private key by entering the PIN again in order to derive the public key with which to access the wallet keys in-app. Theory is that if you lost your phone even if it was rooted by someone who finds it, they're going to have a hard time accessing your private keys. 2: UI Fixes in transaction browser 3: Nicer payment notifications for XRPL 4: Changes to debug logging (can be switched on/off globally)
    Signed-off-by: xrpdevs <xrpdevs@xrpdevs.co.uk>

--- app/src/main/java/uk/co/xrpdevs/flarenetmessenger/ViewContact.java
+++ app/src/main/java/uk/co/xrpdevs/flarenetmessenger/ViewContact.java
@@ -361,6 +361,8 @@ public class ViewContact extends AppCompatActivity implements Button.OnClickList
             bob.cNameText = cNameText;
             bob.start();
         } else {
+
+
             showDialog("Incorrect PIN", true);
         }
     }
